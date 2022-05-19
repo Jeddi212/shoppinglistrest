27e76ed5703c6ad01cfd9c3cf60eb6504dd59af5
Jeddi212 <jedediahf51@gmail.com>
2022-05-19 12:11:44 +0700

    Initial commit

---
+++ src/main/java/com/jeddi/shoppinglistrest/ShoppinglistrestApplication.java
@@ -0,0 +1,13 @@
+package com.jeddi.shoppinglistrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppinglistrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppinglistrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeddi/shoppinglistrest/ShoppinglistrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeddi.shoppinglistrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppinglistrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
